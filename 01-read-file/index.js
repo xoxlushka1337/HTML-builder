1eab182d74954f38787e5297bd51541f75c40793
xoxlushka1337 <xoxlushka@gmail.com>
2023-05-05 19:03:17 +0300

    fix: task 01 readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+const readStream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
 
 readStream.on('data', (chunk) => {
   console.log(chunk.toString());
